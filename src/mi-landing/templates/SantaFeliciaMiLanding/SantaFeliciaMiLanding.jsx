4a5cb1e4cd14b43551342199759f92f5076b719e
CristianVR360 <cristiantrivinom@gmail.com>
2024-07-19 16:29:54 -0400

    Santa felicia

--- src/mi-landing/templates/SantaFeliciaMiLanding/SantaFeliciaMiLanding.jsx
+++ src/mi-landing/templates/SantaFeliciaMiLanding/SantaFeliciaMiLanding.jsx
@@ -4,10 +4,25 @@ import MiLanding from '../../pages/Preview/MiLanding';
 //importacion de recursos
 import logoSantaFelicia from './assets/REDUCCIÓN VERTICAL - BLANCO.png';
 import fav from './assets/FAVICON -NEGRO.png';
-import videoBkg from './assets/bkg.mp4';
+//import videoBkg from './assets/bkg.mp4';
 import imagenBkg from './assets/tolter1.jpg';
+
+import img02 from './assets/img02.jpg';
+import img03 from './assets/img03.jpg';
+import img04 from './assets/img04.jpg';
 import img05 from './assets/img05.jpg';
-import img03 from './assets/img02.jpg';
+import img06 from './assets/img06.jpg';
+import img07 from './assets/img07.jpg';
+import img08 from './assets/img08.jpg';
+import img09 from './assets/img09.jpg';
+import img10 from './assets/img10.jpg';
+import img11 from './assets/img11.jpg';
+import img12 from './assets/img12.jpg';
+import img13 from './assets/img13.jpg';
+import img14 from './assets/img14.jpg';
+import img19 from './assets/img19.jpg';
+import img20 from './assets/img20.jpg';
+
 
 
 const TemplateMiLanding = () => {
@@ -15,8 +30,8 @@ const TemplateMiLanding = () => {
     logo: logoSantaFelicia , // Agrega la ruta de tu logo aquí
     colorPrimario: '#606060',
     colorSecundario: '#fff',
-    colorPrimarioTransparente: 'rgba(96, 96, 96, 0.8)',
-    colorTerciario: '#A2A3A2',
+    colorPrimarioTransparente: 'rgba(162, 163, 162, 0.8)',
+    colorTerciario: '#606060',
     colorCuaternario: '#34495e',
     backgroundImage: imagenBkg ,
     title: 'Parcelas en Toltén',
@@ -38,7 +53,7 @@ const TemplateMiLanding = () => {
     carousels: {
       carousel01:[
       {
-        image:img05 ,
+        image:img04 ,
         videoUrl: "",
         title: 'PARCELAS SANTA FELICIA',
         subtitle: "En la Araucanía costera, a minutos de Queule y Mehuín",
@@ -65,8 +80,8 @@ const TemplateMiLanding = () => {
 
 
       {
-        image: "",
-        videoUrl: videoBkg,
+        image: img13,
+        videoUrl: "",
         title: 'CARACTERÍSTICAS',
         subtitle: 'Detalles únicos en un espacio ideal',
         description: 'Parcelas de 5,000 m², ofrecen amplios espacios ideales para el desarrollo residencial. Ubicadas en una pradera verde con borde de estero, estas parcelas garantizan un entorno natural y fresco, perfecto para aquellos que buscan un estilo de vida en armonía con la naturaleza. Parcelas Santa Felicia asegura la factibilidad de servicios básicos como luz y agua, garantizando que su nueva propiedad esté lista para ser habitada desde el primer día. Además, contamos con caminos de acceso directo a cada parcela, facilitando la llegada y salida de su futuro hogar.',
@@ -78,8 +93,8 @@ const TemplateMiLanding = () => {
       },
 
       {
-        image: "",
-        videoUrl: videoBkg,
+        image: img20,
+        videoUrl: "",
         title: 'PRECIOS Y FINANCIAMIENTO',
         subtitle: 'Desde $18.990.000',
         description: 'Ofrecemos parcelas a precios competitivos y diversas opciones de financiamiento para adaptarse a sus necesidades. Consulte nuestras promociones y descuentos disponibles para facilitar su inversión en este paraíso natural.',
@@ -89,7 +104,133 @@ const TemplateMiLanding = () => {
           buttonText: 'Más informacion',
         },
       },
+    ],
+
+    carousel02:[
+      {
+        image:img19 ,
+        videoUrl: "",
+        title: 'MASTER PLAN 360',
+        subtitle: "Recorre de manera interactiva por nuestro Master Plan 360",
+        description: '',
+        buttonConfig: {
+          type: 'popup',
+          iframeUrl: 'https://astra360.s3.sa-east-1.amazonaws.com/MasterPlan/Santa-Felicia/output/index.html',
+          buttonText: 'Ver Master Plan',
+        },
+      },
+      {
+        image:img19 ,
+        videoUrl: "",
+        title: 'MASTER PLAN 360',
+        subtitle: "Recorre de manera interactiva por nuestro Master Plan 360",
+        description: '',
+        buttonConfig: {
+          type: 'popup',
+          iframeUrl: 'https://astra360.s3.sa-east-1.amazonaws.com/MasterPlan/Santa-Felicia/output/index.html',
+          buttonText: 'Ver Master Plan',
+        },
+      },
+    ],
+
+    carousel03:[
+      {
+        image: img02,
+        videoUrl: "",
+        title: 'PARCELAS SANTA FELICIA',
+        subtitle: "",
+        description: '',
+      },
+      {
+        image: img03,
+        videoUrl: "",
+        title: 'PARCELAS SANTA FELICIA',
+        subtitle: "",
+        description: '',
+      },
+      {
+        image: img04,
+        videoUrl: "",
+        title: 'PARCELAS SANTA FELICIA',
+        subtitle: "",
+        description: '',
+      },
+      {
+        image: img05,
+        videoUrl: "",
+        title: 'PARCELAS SANTA FELICIA',
+        subtitle: "",
+        description: '',
+      },
+      {
+        image: img06,
+        videoUrl: "",
+        title: 'PARCELAS SANTA FELICIA',
+        subtitle: "",
+        description: '',
+      },
+      {
+        image: img07,
+        videoUrl: "",
+        title: 'PARCELAS SANTA FELICIA',
+        subtitle: "",
+        description: '',
+      },
+      {
+        image: img08,
+        videoUrl: "",
+        title: 'PARCELAS SANTA FELICIA',
+        subtitle: "",
+        description: '',
+      },
+      {
+        image: img09,
+        videoUrl: "",
+        title: 'PARCELAS SANTA FELICIA',
+        subtitle: "",
+        description: '',
+      },
+      {
+        image: img10,
+        videoUrl: "",
+        title: 'PARCELAS SANTA FELICIA',
+        subtitle: "",
+        description: '',
+      },
+      {
+        image: img11,
+        videoUrl: "",
+        title: 'PARCELAS SANTA FELICIA',
+        subtitle: "",
+        description: '',
+      },
+      {
+        image: img12,
+        videoUrl: "",
+        title: 'PARCELAS SANTA FELICIA',
+        subtitle: "",
+        description: '',
+      },
+      {
+        image: img13,
+        videoUrl: "",
+        title: 'PARCELAS SANTA FELICIA',
+        subtitle: "",
+        description: '',
+      },
+      {
+        image: img14,
+        videoUrl: "",
+        title: 'PARCELAS SANTA FELICIA',
+        subtitle: "",
+        description: '',
+      },
+      
+
     ]
+
+
+
     }
   
   };
@@ -98,6 +239,7 @@ const TemplateMiLanding = () => {
     { id: 'inicio', texto: 'Inicio' },
     { id: 'carousel01', texto: 'Brochure' },
     { id: 'carousel02', texto: 'Master Plan' },
+    { id: 'carousel03', texto: 'Galería' },
   ];
 
   useEffect(() => {
