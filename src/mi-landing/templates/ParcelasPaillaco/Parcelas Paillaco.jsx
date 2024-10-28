404c4d288dbeef054a885116c8375e83fbb65dd9
CristianVR360 <cristiantrivinom@gmail.com>
2024-10-28 01:05:45 -0300

    logo pailalco

--- src/mi-landing/templates/ParcelasPaillaco/Parcelas Paillaco.jsx
+++ src/mi-landing/templates/ParcelasPaillaco/Parcelas Paillaco.jsx
@@ -9,7 +9,7 @@ import favIcon from './assets/favicon.png' ;
 
 
 ///a
-import Logo from './assets/Logo.png';
+import Logo from './assets/logowhite-8.png';
 import img1 from './assets/img1.jpg';
 import img2 from './assets/img2.jpg';
 import img3 from './assets/img3.jpg';
